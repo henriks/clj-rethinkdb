aeec1db5d81a83ebe3419949d336d5d23c76f6bf
Henrik Saksela <henrik.saksela@gmail.com>
2015-11-15 20:40:25 +0200

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.apa512/rethinkdb "0.11.0"
+(defproject com.apa512/rethinkdb "0.11.1-async"
   :description "RethinkDB client"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
